97141343eb6e3fd633709d62f6cdb052538a68ba
cpavelsek <chrispavelsek@gmail.com>
2021-05-02 13:05:38 -0500

    Updated with Swipe Gestures

--- Lesson5LabB/MainPage.xaml.cs
+++ Lesson5LabB/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+
 namespace Lesson5LabB
 {
     public partial class MainPage : ContentPage
@@ -13,6 +14,8 @@ namespace Lesson5LabB
         public MainPage()
         {
             InitializeComponent();
+
+            //BindingContext = new MainPage();
         }
 
         void OnToggled(object sender, ToggledEventArgs e)
@@ -53,9 +56,56 @@ namespace Lesson5LabB
 
             double testPercentage = (testResults / 5) * 100;
 
-            questionResults.Text = $"{testPercentage}% : You got {testResults} answers correct out of five ";
+            questionResults.Text = $"{testPercentage}% : You got {testResults} answers correct out of five! ";
 
 
+        }
+
+        void SwipeItem_InvokedOne(System.Object sender, System.EventArgs e)
+        {
+            //DisplayAlert("Swipe!", "Inovked", "Okay");
+
+            
+            SwipeAnswerOne.Text = "true!";
+
+            //SwipeAnswer.Text;
+
+
+        }
+
+        //var swipeGesture = new SwipeGestureRecognizer { Direction = SwipeDirection.Left | SwipeDirection.Right };
+
+        void SwipeGestureRecognizer_Swiped(System.Object sender, Xamarin.Forms.SwipedEventArgs e)
+        {
+            switch (e.Direction)
+            {
+                case SwipeDirection.Left:
+                    SwipeAnswerOne.Text = "Please do something";
+                    break;
+            }
+            
+
+
+        }
+
+        
+        void OnSwiped(object sender, SwipedEventArgs e)
+        {
+            switch (e.Direction)
+            {
+                case SwipeDirection.Left:
+                    // Handle the swipe
+                    break;
+                case SwipeDirection.Right:
+                    // Handle the swipe
+                    break;
+                case SwipeDirection.Up:
+                    // Handle the swipe
+                    break;
+                case SwipeDirection.Down:
+                    // Handle the swipe
+                    break;
+            }
         }
     }
 }
